d76c08dbe3ea4c618ed2c51f3038ab821728cbf9
TomFirth <tom.firth84@gmail.com>
2024-10-18 16:44:14 +0100

    initial

---
+++ scripts/barcode.ts
@@ -0,0 +1,4 @@
+// First Number: Product Type. The product type is typically denoted by 0,1,6,7 or 8.
+// Following 5 Numbers: The Manufacturer Code. The five numbers are a unique code that identifies the manufacturer or distributor of the product.
+// Following 5 Numbers on the Right: Product Code. This part of the code is unique to the individual product.
+// Final Number: Check Digit (a Self-Policing System). The final digit of a barcode number is a computer check digit which makes sure the barcode is correctly composed.
